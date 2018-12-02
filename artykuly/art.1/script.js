3c1f76006925a9d492250e75964705613b76348c
Broccoli <thelubb1@gmail.com>
2018-12-02 12:22:19 +0100

    Add files via upload

---
+++ artykuly/art.1/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+
+  $(window).on("scroll", () => {
+
+    let scroll =  $(document).scrollTop();
+
+    if( scroll <= $(".pic").height() ){
+      $(".glow").css({
+      })
+    }
+
+  })
+
+
+
+
+
+})
