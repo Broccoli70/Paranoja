ac20474e52d07d472ca939a818305484964e2c62
Broccoli <thelubb1@gmail.com>
2018-12-06 21:03:53 +0100

    Add files via upload

---
+++ artykuly/tworcy/script.js
@@ -0,0 +1,45 @@
+// $(function(){
+//
+//
+//   $(window).on("scroll", () => {
+//
+//     let scroll =  $(document).scrollTop();
+//     let picHeight = $(".pic").height()/8 ;
+//
+//     if(scroll > picHeight*3) {
+//       $("#name1")
+//       .animate({
+//         opacity: 1,
+//         marginTop: 0,
+//
+//       }, 2000)
+//
+//       $("#face1")
+//       .delay(500)
+//       .animate({
+//         opacity: 1,
+//
+//       }, 2000)
+//     }
+//
+//     if(scroll > picHeight*6) {
+//       $("#name2")
+//       .animate({
+//         opacity: 1,
+//         marginTop: 0,
+//
+//       }, 2000)
+//
+//       $("#face2")
+//       .delay(500)
+//       .animate({
+//         opacity: 1,
+//
+//       }, 2000)
+//     }
+//
+//
+//
+//
+//   })
+// })
